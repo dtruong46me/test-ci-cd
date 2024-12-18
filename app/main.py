5f586e1c399c22d497c737c967efcb5406e505f1
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2024-12-18 15:15:39 +0700

    Add subtraction function and corresponding tests

--- app/main.py
+++ app/main.py
@@ -2,5 +2,8 @@
 def add(a, b):
     return a + b
 
+def substract(a, b):
+    return a - b
+
 if __name__=="__main__":
     print(f"The result is: {add(2, 3)}")
--- test/test_main.py
+++ test/test_main.py
@@ -5,13 +5,17 @@ import os, sys
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, path)
 
-from app.main import add
+from app.main import add, substract
 
 class TestMain(unittest.TestCase):
     def test_main(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+
+        self.assertEqual(substract(2, 3), -1)
+        self.assertEqual(substract(-1, 1), -2)
+        self.assertEqual(substract(0, 0), 0)
 
 if __name__ == '__main__':
     unittest.main()
